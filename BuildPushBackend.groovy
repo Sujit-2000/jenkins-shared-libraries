7b56a59f5193b9215490504fb0b66f9252cea3a6
Sujit-2000 <sujitkumarkadam442@gmail.com>
2025-08-08 12:18:24 +0530

    Delete BuildPushBackend.groovy

--- BuildPushBackend.groovy
+++
@@ -1,8 +0,0 @@
-def call(String dockerhubCreds, String backendImage) {
-    stage('Build & Push Backend Image') {
-        withDockerRegistry(credentialsId: dockerhubCreds, url: '') {
-            sh "docker build -t ${backendImage} ."
-            sh "docker push ${backendImage}"
-        }
-    }
-}
